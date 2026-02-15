74b718c00d48434e5b1b2223118bfc1f5b12add0
RogerBFPV <34823708+RogerBFPV@users.noreply.github.com>
2026-02-15 14:08:16 -0500

    Avoid starting websocket client in runserver autoreload parent process

--- race/apps.py
+++ race/apps.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from django.apps import AppConfig
 
 
@@ -6,6 +9,11 @@ class RaceConfig(AppConfig):
     name = "race"
 
     def ready(self) -> None:
+        # Django runserver with autoreload starts a parent + child process.
+        # Only start the Velocidrone client in the serving child process.
+        if "runserver" in sys.argv and os.environ.get("RUN_MAIN") != "true":
+            return
+
         from .state import get_machine_ip
         from .velocidrone import client
 
